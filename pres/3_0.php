c09c008fa8eb54129d6457d76ae21444965bae34
s-79 <sebastien.trouve@mailo.com>
2021-09-20 09:47:57 +0200

    DRIEETS

--- pres/3_0.php
+++ pres/3_0.php
@@ -5,10 +5,10 @@
         <h2 class="text-center text-uppercase fw-bold mt-2 bleu"><span id="fg3_0"><i class="fas fa-arrow-alt-circle-left fa-lg pointeur liens pe-3"></i></span>Les pilotes<span id="fd3_0"><i  class="fas fa-arrow-alt-circle-right fa-lg pointeur liens ps-3"></i><span></h2><br>
     </div>
     <div class="contenuDispositif pilotes d-flex justify-content-between my-auto">
-        <a href="http://www.seine-saint-denis.gouv.fr/Services-de-l-Etat/Sante-cohesion-sociale-et-droit-des-femmes/Direction-departementale-de-la-cohesion-sociale/Direction-departementale-de-la-cohesion-sociale-DDCS-presentation" target="_blank" class="my-auto">
+        <a href="https://seinesaintdenis.fr/" target="_blank" class="my-auto">
         <img class="img-pilotes img-fluid" src="img/logo/CD.png"></a>
         <a href="http://www.caf.fr/" target="_blank" class="my-auto"><img class="img-pilotes img-fluid" src="img/logo/caf.png"></a>
-        <a href="https://seinesaintdenis.fr/" target="_blank" class="my-auto"><img class="img-pilotes img-fluid" src="img/logo/DSDEN.jpg"></a>
+        <a href="https://idf.drieets.gouv.fr/" target="_blank" class="my-auto"><img class="img-pilotes img-fluid" src="img/logo/DRIEETS.jpg"></a>
 
     </div>
 </div>
