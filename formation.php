b28c5e5c2008bf8e12ab7ff3feff95e86a928e43
s-79 <sebastien.trouve@mailo.com>
2021-10-13 09:48:08 +0200

    Formation

---
+++ formation.php
@@ -0,0 +1,130 @@
+<?php
+include("header.php");
+?>
+
+<section class="mx-auto" style="background-color:white;">
+	<div class="divTitre">
+		<h1 class="bleu">FORMATIONS DES 11 ET 12 OCTOBRE 2021</h1>
+	</div>
+
+    <div class="orange-divider" style="width: 400px;"> </div>
+    <p style="background-color:white!"><br>Bonjour à toutes et à tous,<br><br>
+
+J'ai été informé avant-hier que la salle initialement prévue pour les 2 journées de formation à Bobigny n'était plus disponible.<br><br>
+
+L'équipe du 36 au Pré-Saint-Gervais a spontanément proposée de nous accueillir pour la journée du lundi et je les remercie sincèrement pour cette proposition.<br>
+Pour les formations du mardi, nous louerons une salle auprès des Relais Solidaires de Pantin.<br><br>
+
+Vous trouverez les lieux et adresses exactes des formations dans le tableau ci-dessous.<br>
+Je suis vraiment désolé pour ces changements de dernière minute et j'ai bien conscience qu'ils peuvent rendre plus compliqués vos déplacements pour vous rendre aux formations.<br><br>
+
+J'ai ajouté une colonne au tableau dans laquelle vous trouverez des liens pour pouvoir (si possible) suivre les formations à distance.<br>
+Je vais essayer de mettre en place cette option pour celles et ceux qui ne pourraient vraiment pas se rendre sur les lieux de formation.<br>
+Le fonctionnement effectif et concret de la visio dépendra des modalités des lieux et des interventions, aussi je vous encourage très fortement à vous rendre sur place.</p>
+
+	<div class="my-4">
+        <b>LES LIEUX DE FORMATION ET LES LIENS VISIO</b><br><br>
+        <table width="100%" cellspacing="2" cellpadding="2" border="1" style="background-color:white;">
+            <tbody>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" bgcolor="#e85017" align="center"><font size="-1"><b><font color="#ffffff">DATE<br>
+                    </font></b></font></td>
+                    <td style="outline: thin solid" valign="middle" bgcolor="#e85017" align="center"><font size="-1"><b><font color="#ffffff">LIEU<br>
+                    </font></b></font></td>
+                    <td style="outline: thin solid" valign="middle" bgcolor="#e85017" align="center"><font size="-1"><b><font color="#ffffff">ADRESSE<br>
+                    </font></b></font></td>
+                    <td style="outline: thin solid" valign="middle" bgcolor="#e85017" align="center"><font size="-1"><b><font color="#ffffff">À DISTANCE<br>
+                    </font></b></font></td>
+                </tr>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Lundi 11 octobre<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1"><b>Le 36</b><br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">36 Rue André Joineau 93310 Le PRÉ-SAINT-GERVAIS<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1"><a moz-do-not-send="true"
+                    href="https://us02web.zoom.us/j/86352310425?pwd=SS9hNEQyb0JRVGtVak83blRreFhGZz09">Lien vers la visio</a><br>
+                    ID : 863 5231 0425<br>
+                        Code : 095654<br>
+                    </font> </td>
+                </tr>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1"><font size="-1">Mardi 12 octobre</font></font> </td>
+                    <td style="outline: thin solid"  border="1" valign="middle" align="center"><font size="-1"><b>Les Relais Solidaires</b><br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">61 rue Victor Hugo 93500 PANTIN</font><font
+                    size="-1"><br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1"><a moz-do-not-send="true"
+                    href="https://us02web.zoom.us/j/88221360137?pwd=MktKaE5JZnluakgyMGJOUWlJaTdrQT09">Lien vers la visio</a><br>
+                    ID : 882 2136 0137<br>
+                        Code : 392737<br>
+                    </font> </td>
+                </tr>
+            </tbody>
+        </table>
+
+	<div class="my-4">
+        <b>LE PROGRAMME</b><br><br>
+        <table width="100%" cellspacing="2" cellpadding="2" border="1" style="background-color:white;">
+            <tbody>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" bgcolor="#133f6f" align="center"><font size="-1"><b><font color="#ffffff">DATE<br>
+                    </font></b></font></td>
+                    <td style="outline: thin solid" valign="middle" bgcolor="#133f6f" align="center"><font size="-1"><b><font color="#ffffff">HEURES<br>
+                    </font></b></font></td>
+                    <td style="outline: thin solid" valign="middle" bgcolor="#133f6f" align="center"><font size="-1"><b><font color="#ffffff">INTITULÉ<br>
+                    </font></b></font></td>
+                </tr>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Lundi 11 octobre<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">de 10h à 13h00<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Présentation des outils et ressources disponibles +
+                    temps d'échange<br>
+                    </font> </td>
+                </tr>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Lundi 11 octobre</font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">de 14h à 17h30<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Panorama des usages numériques des jeunes et
+                    parentalité à l'ère du numérique - <b>e-Enfance</b><br>
+                    </font> </td>
+                </tr>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Mardi 12 octobre</font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">de 10h à 13h30<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Focus sur la ligne 3018 (NetEcoute) avec études de cas
+                    anonymisés - <b>e-Enfance</b></font> </td>
+                </tr>
+                <tr style="outline: thin solid">
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Mardi 12 octobre</font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">de 15h à 18h30<br>
+                    </font> </td>
+                    <td style="outline: thin solid" valign="middle" align="center"><font size="-1">Internet, image de soi, rapport au corps et sexualité
+                    des ados - <b>e-Enfance</b></font> </td>
+                </tr>
+            </tbody>
+        </table>
+	</div>
+    Au plaisir de vous y retrouver !<br>
+<br>
+N'hésitez pas à me contacter pour toute question ou demande d'information.<br>
+<br>
+Bonne journée et à lundi.<br>
+<br>
+<b>Sébastien TROUVÉ</b><br>
+Chargé de mission numérique éducatif<br>
+Co-coordinateur du réseau PDN 93<br>
+07 66 10 17 28<br>
+<b><a href="https://pdn.ligue93.org/" style="color:#e85017;text-decoration:none;">www.pdn.ligue93.org</a><br>
+  ................................................</b><br>
+Ligue de l'enseignement - F.O.L. 93<br>
+119, rue Pierre Sémard<br>
+93000 BOBIGNY
+
+</section>
