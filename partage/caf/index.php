8dd276d5b6fdb9fecfb17e1d01504c4c43b27de8
s-79 <sebastien.trouve@mailo.com>
2022-06-25 10:49:59 +0200

    maj

---
+++ partage/caf/index.php
@@ -0,0 +1,4 @@
+<?php
+    header("Location: https://pdn.ligue93.org/partage/caf/diapo.sozi.html");
+	die();
+?>
--- partage/pdn/index.php
+++ partage/pdn/index.php
@@ -1,4 +1,4 @@
 <?php
-    header("Location: https://pdn.ligue93.org/partage/pdn/diapo.sozi.html#frame7609");
+    header("Location: https://pdn.ligue93.org/partage/pdn/caf");
 	die();
 ?>
